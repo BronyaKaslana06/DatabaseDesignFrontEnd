3c1190d807f84a59254579de5cff8319db7a6663
afishspring <yuchunxianphy@163.com>
2023-07-05 14:05:48 +0800

    删除vueapp/obj
    临时文件，无需上传

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
         },
         proxy: {
             '^/weatherforecast': {
-                target: 'https://localhost:7244/'
+                target: 'https://localhost:7240/'
             }
         },
         port: 5002
