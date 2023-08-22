0da7d687b99ed98073427ba1b289371898313df5
klmt12138 <86057358+klmt12138@users.noreply.github.com>
2023-08-22 20:45:58 +0800

    Update sliderbar-nav.vue

--- src/components/sliderbar-nav.vue
+++ src/components/sliderbar-nav.vue
@@ -77,6 +77,16 @@ const owner_items = [
     index: '/repair',
     title: '维修服务'
   },
+  {
+    name: '公告信息',
+    index: '/employee-notice-page',
+    title: '公告信息'
+  },
+  {
+    name: '个人信息',
+    index: '/personal-information-page',
+    title: '个人信息'
+  },
 ]
 
 const staff_items = [
