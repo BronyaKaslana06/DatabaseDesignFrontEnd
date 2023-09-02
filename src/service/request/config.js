3ffc355f7cfbff3a66d5dd2b2132287a50e41dea
afishspring <104251938+afishspring@users.noreply.github.com>
2023-09-02 21:55:22 +0800

    前端部署的配置

--- src/service/request/config.js
+++ src/service/request/config.js
@@ -3,7 +3,7 @@ let port = window.location.port;
 if (process.env.NODE_ENV === 'development') {
     BASE_URL = 'https://localhost:'+port;
 } else if (process.env.NODE_ENV === 'production') {
-    BASE_URL = 'https://a9ccf582-e1ef-4d00-891a-4e40f24c2459.mock.pstmn.io';
+    BASE_URL = 'http://8.130.9.72:'+port;
 } else {
     BASE_URL = 'https://a9ccf582-e1ef-4d00-891a-4e40f24c2459.mock.pstmn.io';
 }
